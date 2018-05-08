a9fe1e849b4a8a454ab958c7f3edd34a119d3e6a
MichaelBorrasso <michaelborrasso@gmail.com>
2018-05-08 16:06:13 -0400

    server is live, but is not loading in browser

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+var friendsArray =
+    {
+        name: "",
+        photo: "",
+        scores: [, , , , , , , , ,]
+    };
+
+module.exports = friendsArray;
+
+
+//Could this be coded more simply by:
+// module.exports = [];
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+var path = require("path");
+
+module.exports = function (app) {
+
+    //Survey
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname + "../public/survey.html"));
+    });
+
+    //Home
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname + "../public/home.html"));
+    });
+}
